81b287e7d3d235febfa83c5b968a7a664f3f9b5d
Dan Eagle <deagle1226@gmail.com>
2017-06-01 10:21:55 -0700

    debug optional

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import GameContainer from '../containers/GameContainer'
 
-export default () => (
-    <GameContainer debug={true}>
-        {gameTime => (
-            <h1>Credit Clicker</h1>
-        )}
-    </GameContainer>
-)
+export default (props) => {
+    return (
+        <GameContainer debug={props.url.query.debug}>
+            {gameTime => (
+                <h1>Credit Clicker</h1>
+            )}
+        </GameContainer>
+    )
+}
