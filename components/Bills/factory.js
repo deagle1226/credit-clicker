c506330d5725ce5b75c9f1abb0ad162dc1593613
Dan Eagle <deagle1226@gmail.com>
2017-06-01 12:05:11 -0700

    bills

--- components/Bills/factory.js
+++
@@ -1,8 +0,0 @@
-import Bill from './model'
-
-export default function billsFactory(bills, gameTime) {
-    if (gameTime % 1000 < 16) {
-        bills.push(new Bill(gameTime))
-    }
-    return bills
-}
