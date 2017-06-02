df34f4fd24af3814af2166354bb5d08eb481f29f
Dan Eagle <deagle1226@gmail.com>
2017-06-01 18:20:38 -0700

    save/load profiles

--- components/Game.js
+++ components/Game.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import store from 'store'
 import ScoreDial from '@ck/score-dial'
 import { RATINGS } from '@ck/score-dial/lib/constants'
 import Bill from './Bill'
@@ -15,7 +17,7 @@ import inRange from 'lodash/inRange'
 import { totalCardsUtilization } from '../utils/creditTools'
 import { scoreDelta } from '../utils/creditTools'
 
-export default class GameState extends Component {
+class GameState extends Component {
     constructor(props) {
         super(props)
         this.state = {
@@ -38,15 +40,37 @@ export default class GameState extends Component {
         this.payCardBalance = this.payCardBalance.bind(this)
         this.defectBill = this.defectBill.bind(this)
         this.selectActiveCard = this.selectActiveCard.bind(this)
+        this.load = this.load.bind(this)
+        this.save = this.save.bind(this)
+    }
+
+    componentDidMount() {
+        this.load()
     }
 
     componentWillReceiveProps(nextProps) {
         const bills = Bill.factory(this.state.bills, nextProps.gameTime)
-        this.setState({ bills })
+        setTimeout(() => this.setState({ bills }), 1)
         daily(nextProps.gameTime, () => {
             this.updateFinances('cash', this.state.job.salary)
-            this.updateCredit('score', scoreDelta(this.state.cards))
+            setTimeout(() => this.updateCredit('score', scoreDelta(this.state.cards)), 1)
         })
+    }
+
+    save() {
+        const profile = this.context.profile
+        store.set(`CK_Clicker_profiles-${profile}`, this.state)
+        store.set(`CK_Clicker_profiles-${profile}-time`, this.props.gameTime)
+    }
+
+    load() {
+        const profile = this.context.profile
+        const state = store.get(`CK_Clicker_profiles-${profile}`)
+        if (state) {
+            this.setState(state)
+        } else {
+            console.warn('no save data for ', `CK_Clicker_profiles-${profile}`)
+        }
     }
 
     updateCredit(key, delta) {
@@ -150,6 +174,9 @@ export default class GameState extends Component {
                         <Bill.Component bills={bills} pay={this.payBill} defect={this.defectBill} gameTime={gameTime} />
                     )}
                 />
+                <button style={{ position: 'absolute', right: 0, top: 15 }} onClick={this.save}>
+                    save
+                </button>
                 <style jsx>{`
                     .header {
                         display: flex;
@@ -162,3 +189,9 @@ export default class GameState extends Component {
         )
     }
 }
+
+GameState.contextTypes = {
+    profile: PropTypes.string
+}
+
+export default GameState
---
+++ containers/ProfileContainer.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import store from 'store'
+import concat from 'lodash/concat'
+import map from 'lodash/map'
+
+class ProfileContainer extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            profile: null,
+            newProfile: null
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.selectProfile = this.selectProfile.bind(this)
+    }
+
+    getChildContext() {
+        return { profile: this.state.profile }
+    }
+
+    componentDidMount() {
+        this.setState({ availableProfiles: store.get('CK_Clicker_profiles') })
+    }
+
+    handleChange(event) {
+        this.setState({ newProfile: event.target.value })
+    }
+
+    handleSubmit(event) {
+        this.setState({ profile: this.state.newProfile }, () => {
+            const previousProfiles = store.get('CK_Clicker_profiles')
+            const profiles = concat(previousProfiles || [], [this.state.newProfile])
+            store.set('CK_Clicker_profiles', profiles)
+        })
+    }
+
+    selectProfile(profile) {
+        this.setState({ profile })
+    }
+
+    render() {
+        if (this.state.profile) {
+            return (
+                <div>
+                    <i style={{ position: 'absolute', top: 0, right: 0 }}>{this.state.profile}</i>
+                    {this.props.children}
+                </div>
+            )
+        }
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    New Profile: <input type="text" name="profile" onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Enter" />
+                <h3>Profiles:</h3>
+                <div>
+                    {map(this.state.availableProfiles, (profile, idx) => (
+                        <button key={idx} onClick={() => this.selectProfile(profile)}>{profile}</button>
+                    ))}
+                </div>
+            </form>
+        )
+    }
+}
+
+ProfileContainer.childContextTypes = {
+    profile: PropTypes.string
+}
+
+export default ProfileContainer
