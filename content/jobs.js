914d45942973ce11175fade73deacc5c7f0acb12
Dan Eagle <deagle1226@gmail.com>
2017-06-02 11:49:47 -0700

    some more jobs

--- content/jobs.js
+++ content/jobs.js
@@ -1,15 +1,78 @@
+export const UltraBusinessOwner = {
+    title: 'Ultra Business Owner',
+    wage: 50,
+    salary: 10000,
+    promotions: []
+}
+
+export const MaximumBusinessOwner = {
+    title: 'Maximum Business Owner',
+    wage: 20,
+    salary: 5000,
+    promotions: [
+        {
+            job: UltraBusinessOwner,
+            cost: {
+                cash: 5000,
+                credit: 750,
+                time: 50000
+            }
+        }
+    ]
+}
+
+export const MediumBusinessOwner = {
+    title: 'Medium Business Owner',
+    wage: 15,
+    salary: 2000,
+    promotions: [
+        {
+            job: MaximumBusinessOwner,
+            cost: {
+                cash: 2000,
+                credit: 700,
+                time: 25000
+            }
+        }
+    ]
+}
+
+export const UltraWageEarner = {
+    title: 'Ultra Wage Earner',
+    wage: 100,
+    salary: 1000,
+    promotions: []
+}
+
 export const MaximumWageEarner = {
     title: 'Maximum Wage Earner',
     wage: 20,
     salary: 2,
-    promotions: []
+    promotions: [
+        {
+            job: UltraWageEarner,
+            cost: {
+                cash: 1500,
+                time: 15000
+            }
+        }
+    ]
 }
 
 export const MinimumBusinessOwner = {
     title: 'Minimum Business Owner',
     wage: 10,
     salary: 200,
-    promotions: []
+    promotions: [
+        {
+            job: MediumBusinessOwner,
+            cost: {
+                cash: 1000,
+                credit: 650,
+                time: 15000
+            }
+        }
+    ]
 }
 
 export const MediumWageEarner = {
