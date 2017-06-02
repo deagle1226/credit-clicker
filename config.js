1260571215f5d41a4bf4b0abc409d5254408b291
Ryan Nordman <spacerobots@gmail.com>
2017-06-02 09:29:37 -0700

    Doubled the amount of time before bills become overdue

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ export default {
     bill: {
         spawnRate: DAY * 0.4,
         amount: (gameTime) => Math.round(gameTime / 500),
-        due: (gameTime) => gameTime + (12 * HOUR)
+        due: (gameTime) => gameTime + (24 * HOUR)
     }
 }
